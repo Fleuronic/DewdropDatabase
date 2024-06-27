e6c8aa93d0f7459c8023b0d6ba3b8b2c1c3cbe14
Jordan Kay <jordan@fleuronic.com>
2024-06-27 10:49:33 -0700

    Add group model

---
+++ Sources/DewdropDatabase/Database/Models/Group.swift
@@ -0,0 +1,25 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import Schemata
+
+import struct Dewdrop.Group
+import struct DewdropService.IdentifiedGroup
+import struct PersistDB.Ordering
+import protocol PersistDB.Model
+import protocol Catenoid.Model
+
+extension Group.Identified: Schemata.Model, AnyModel {
+	// MARK: Model
+	public static let schema = Schema(
+		Self.init..."groups",
+		\.title * "title"
+	)
+}
+
+// MARK: -
+extension Group.Identified: PersistDB.Model {
+	// MARK: Model
+	public static var defaultOrder: [Ordering<Self>] {
+		[.init(\.id, ascending: false)]
+	}
+}
