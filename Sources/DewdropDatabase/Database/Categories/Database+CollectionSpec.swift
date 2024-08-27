7bb12b1a7151e3a3f7c0b2c8e29a13e82ca7121e
Jordan Kay <jordan@fleuronic.com>
2024-08-27 12:59:02 -0400

    Fix build

--- Sources/DewdropDatabase/Database/Categories/Database+CollectionSpec.swift
+++ Sources/DewdropDatabase/Database/Categories/Database+CollectionSpec.swift
@@ -5,6 +5,9 @@ import protocol Catena.Scoped
 import protocol Catenoid.Database
 
 extension Database: CollectionSpec {
+	// TODO: Remove
+	public typealias RootCollectionListFields = CollectionListFields
+
 	public func listRootCollections() async -> Self.Result<[CollectionListFields]> {
 		await fetch(where: .isRoot)
 	}
