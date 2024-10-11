17ebd147d0782a2e26cb4c02135eefb94035a08f
Jordan Kay <jordan@fleuronic.com>
2024-10-11 15:21:39 -0400

    Remove unused typealias

--- Sources/DewdropDatabase/Database/Categories/Database+CollectionSpec.swift
+++ Sources/DewdropDatabase/Database/Categories/Database+CollectionSpec.swift
@@ -6,9 +6,6 @@ import protocol Catena.Scoped
 import protocol Catenoid.Database
 
 extension Database: CollectionSpec {
-	// TODO: Remove
-	public typealias RootCollectionListFields = CollectionListFields
-
 	public func listRootCollections() async -> Self.Result<[CollectionListFields]> {
 		await fetch(where: .isRoot)
 	}
