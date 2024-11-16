e6880e31fed9d0576606d7e5c1e8a512674c8495
Jordan Kay <jordan@fleuronic.com>
2024-11-16 11:26:06 -0500

    Add user row

--- Sources/DewdropDatabase/Database/Rows/UserRow.swift
+++ Sources/DewdropDatabase/Database/Rows/UserRow.swift
@@ -1,8 +1,44 @@
-//
-//  File.swift
-//  DewdropDatabase
-//
-//  Created by Jordan Kay on 11/15/24.
-//
+// Copyright © Fleuronic LLC. All rights reserved.
 
-import Foundation
+import Schemata
+import PersistDB
+
+import struct Dewdrop.User
+import struct Foundation.URL
+import protocol Catenoid.Fields
+import protocol Catenoid.Model
+import protocol DewdropService.UserAuthenticatedFields
+
+struct UserRow: UserAuthenticatedFields {
+	let id: User.ID
+	let fullName: String
+	let email: String?
+	let avatarURL: URL?
+	let hasProSubscription: Bool?
+}
+
+// MARK
+extension UserRow: Fields {
+	// MARK: ModelProjection
+	static let projection = Projection<Self.Model, Self>(
+		Self.init,
+		\.id,
+		\.value.fullName,
+		\.value.email,
+		\.value.avatarURL,
+		\.value.hasProSubscription
+	)
+}
+
+// MARK: -
+extension UserRow: Catenoid.Model {
+	// MARK: Model
+	public var valueSet: ValueSet<User.Identified> {
+		[
+			\.value.fullName == fullName,
+			\.value.email == email,
+			\.value.avatarURL == avatarURL,
+			\.value.hasProSubscription == hasProSubscription
+		]
+	}
+}
